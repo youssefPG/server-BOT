50c894aa45f3f4bd9eb6ad06aee77070a8686a26
youssefmoutik <>
2021-09-24 14:28:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import requests
+import json
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('we have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if msg.startswith('$hi'):
+    await message.channel.send('hello!')
+
+  if msg.startswith('$hi bot how are you'):
+    await message.channel.send('i am fine thank you ask!!')
+
+keep_alive()
+client.run('ODkwNjUzNDg3ODIwNTc0NzQx.YUy7sw.Eu_5dNTWDMytjePpzgSX_zFsiGs')
